3b08eb3bdb8e385bfd27c28a34f7f1eb8bfb0f2e
wujinhjun <wujinhjun@sjtu.edu.cn>
2022-11-12 09:38:54 +0800

    feat: add routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import Header from "./components/Header";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, HashRouter, Route, Routes } from "react-router-dom";
 import FindMusic from "./components/FindMusic";
 import { Fragment } from "react";
+import MyMusic from "./components/MyMusic";
+import Friend from "./components/Friend";
+import Download from "./components/Download";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
-        <Route path="/" exact element={<FindMusic />}></Route>
+        <Route path="/" exact element={<FindMusic />} />
+        <Route path="/my" exact element={<MyMusic />} />
+        <Route path="/friend" exact element={<Friend />} />
+        <Route path="/download" exact element={<Download />} />
       </Routes>
       <Fragment>
         <div style={{ height: "1000px" }}></div>
       </Fragment>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
